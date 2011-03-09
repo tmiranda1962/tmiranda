2f29dbfad224c355dd6b70abcfd9ad20f18a05a1
tom.miranda <tom.miranda@gmail.com>
2011-03-09 22:23:35 +0000

    Last version before MultiLogon.

--- MultiUserSupport/src/tmiranda/mus/API.java
+++ MultiUserSupport/src/tmiranda/mus/API.java
@@ -1271,6 +1271,32 @@ public class API {
     }
 
     /**
+     * Add the Favorite for the specified UserID.
+     * @param UserID
+     * @param Favorite
+     */
+    public static void addFavorite(String UserID, Object Favorite) {
+
+        if (UserID==null || UserID.equalsIgnoreCase(Plugin.SUPER_USER)) {
+            Log.getInstance().write(Log.LOGLEVEL_TRACE, "addFavorite: Adding Favorite for all Users.");
+
+            List<String> allUsers = User.getAllUsers(false);
+
+            for (String thisUser : allUsers) {
+                MultiFavorite MF = new MultiFavorite(thisUser, Favorite);
+                MF.addFavorite();
+            }
+
+            return;
+        }
+
+        Log.getInstance().write(Log.LOGLEVEL_TRACE, "addFavorite: Adding Favorite for User " + UserID);
+        MultiFavorite MF = new MultiFavorite(UserID, Favorite);
+        MF.addFavorite();
+        return;
+    }
+
+    /**
      * Invoke IN PLACE OF core API.
      * @param Airing
      * @return
